c9c2a5d507dc5bd6f95ecc0792ec9197656a24f2
dafengniao <yongfeng.me@gmail.com>
2020-08-26 20:03:59 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}123</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
-      check out the
+      check out the123123123
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
@@ -35,6 +35,9 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  created () {
+    console.log(11111)
   }
 }
 </script>
